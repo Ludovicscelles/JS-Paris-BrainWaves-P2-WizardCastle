1fb0268c75a1d54410df00006093e53dd4697e20
Antoine <ant.delalande@gmail.com>
2024-04-12 17:04:11 +0200

    fixed logo bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   
   return (
     <main>
-    <img src="./assets/wizard_logo_black.svg" alt=""/>
+    <img src="/src/assets/wizard_logo_black.svg" alt=""/>
     <h1>Welcome to Wizard's Tower, dear adventurer !</h1>
     <p>We provide all kind of resources to help you play Dungeons & Dragons !</p>
     </main>
