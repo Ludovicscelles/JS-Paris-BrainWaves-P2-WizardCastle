d2c2597aeb337af6d2d698289816019dd46b3ca8
Antoine <ant.delalande@gmail.com>
2024-04-15 11:19:26 +0200

    added home & rules pages

---
+++ client/src/pages/Rules.jsx
@@ -0,0 +1,10 @@
+function Rules() {
+    return(
+        <>
+            <h1>Rules</h1>
+            <p>You want to learn the rules of Dungeons & Dragons ? Here is everything you need to get started !</p>
+        </>
+    )
+}
+
+export default Rules; 
