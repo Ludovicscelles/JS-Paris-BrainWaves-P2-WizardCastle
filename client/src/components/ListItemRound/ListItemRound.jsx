566602eafb6f7b52885426d1dfe52a9eefd4f7b5
Antoine <ant.delalande@gmail.com>
2024-04-16 17:17:01 +0200

    added fetching of single monster info

--- client/src/components/ListItemRound/ListItemRound.jsx
+++ client/src/components/ListItemRound/ListItemRound.jsx
@@ -1,14 +1,16 @@
 import "./ListItemRound.css"
 import PropTypes from 'prop-types'
 
-function ListItemRound({ itemName }) {
+function ListItemRound({ itemName, setState, itemInfo }) {
     return (
-        <button type="button" className="ListItemRound">{itemName}</button>
+        <button type="button" className="ListItemRound" onClick={() => setState(itemInfo)}>{itemName}</button>
     )
 }
 
 ListItemRound.propTypes = {
     itemName: PropTypes.string.isRequired,
+    setState: PropTypes.func.isRequired,
+    itemInfo: PropTypes.objectOf(PropTypes.string).isRequired,
 }
 
 export default ListItemRound;
--- client/src/pages/Bestiary.jsx
+++ client/src/pages/Bestiary.jsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import ListItemRound from "../components/ListItemRound/ListItemRound";
 import FilterButton from "../components/FilterButton/FilterButton";
 import "./Bestiary.css";
+
 
 function Bestiary() {
   // states
@@ -9,17 +11,28 @@ function Bestiary() {
   const monsterTypes = ["aberration", "beast","celestial", "construct", "dragon", "elemental", "fey", "fiend", "giant", "humanoid", "monstrosity", "ooze", "plant", "undead" ];
   // fonctions
 
+const [monsterInfo, setMonsterInfo] = useState([])
+const [monsterIndex, setMonsterIndex] = useState([])
+
+    useEffect(() => {
+    fetch(`https://www.dnd5eapi.co/api/monsters/${monsterIndex}`)
+    .then((res) => res.json())
+    .then((data) => setMonsterInfo(data))
+    .catch((err) => console.error(err))
+  }, [monsterIndex])
 
 
   // appel du composant
 
   // tests
+  console.info(monsterInfo)
   
   return (
     <>
       <h1>Bestiary</h1>
+      <main>
       <h2>Monsters in D&D</h2>
-      <p>
+      <p className="intro-text">
       In the vast realm of Dungeons & Dragons, where heroes rise and fall in epic quests, there exists a myriad of creatures both wondrous and  terrifying. These beings, known collectively as monsters, inhabit the  deepest dungeons, the darkest forests, and the highest peaks. Some are  born of ancient magic, while others are the twisted creations of dark sorcery.
       </p>
       <h2>Filter by monster type</h2>
@@ -27,9 +40,10 @@ function Bestiary() {
       {monsterTypes.map((type) => (<FilterButton key={type.index} filterType={type}/>))}
       </section>
       <h2>All monsters</h2>
-      <section>
-      {results.map((monster) => (<ListItemRound key={monster.name} itemName={monster.name}/>))}
+      <section className="list-results">
+      {results.map((monster) => (<ListItemRound key={monster.name} itemName={monster.name} setState={setMonsterIndex} itemInfo={monster.index}/>))}
       </section>
+      </main>
     </>
   );
 }
