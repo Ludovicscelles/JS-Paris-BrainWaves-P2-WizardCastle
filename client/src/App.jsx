e0bb1ba028c45bde3cd709d3d57867a4096a603c
vvdam <maaalouladam@gmail.com>
2024-04-16 11:22:58 +0200

    routage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,23 @@
-import "./assets/reset.css"
+import "./assets/reset.css";
+
 import "./App.css";
 import { Link, Outlet } from "react-router-dom";
 
 function App() {
-
-  
-  
   return (
     <>
-    <header>
-      <img src="/src/assets/wizard_logo_black.svg" alt=""/>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/rules">Rules</Link>
-      </nav>
-    </header>
-    <main>
-      <Outlet />
-    </main>
+      <header>
+        <img src="/src/assets/wizard_logo_black.svg" alt="" />
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/rules">Rules</Link>
+          <Link to="/classes">Class</Link>
+          <Link to="/bestiary">bestiary</Link>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
