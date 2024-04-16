4971a49ca073a524ff22134d057497b3cc89daba
Antoine <ant.delalande@gmail.com>
2024-04-16 11:41:04 +0200

    added changes

--- client/src/pages/Rules.jsx
+++ client/src/pages/Rules.jsx
@@ -1,13 +1,16 @@
 import { useLoaderData } from "react-router-dom";
 
 function Rules() {
-    const rule = useLoaderData();
+    const ruleSet = useLoaderData();
+
+    
 
     return(
         <>
             <h1>Rules</h1>
             <p>You want to learn the rules of Dungeons & Dragons ? Here is everything you need to get started !</p>
-            <p>Here is a rule : {rule.results[0].name}</p>
+            {ruleSet.results.map((rule) => <p key={rule.index}>{rule.name}</p>)}
+            <p><a href={`${ruleSet.results[0].url}`}>See rule</a></p>
         </>
     )
 }
