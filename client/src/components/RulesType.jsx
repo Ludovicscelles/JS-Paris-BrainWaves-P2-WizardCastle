1e5b16ac2f99958aa77f20cc8e1ff78fb4131f64
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2024-04-25 11:02:50 +0200

    correction-rulestypes.jsx

--- client/src/components/RulesType.jsx
+++ client/src/components/RulesType.jsx
@@ -1,14 +1,10 @@
 import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
 
-
 function RulesType({ name, url, id }) {
     const [subsection, setSubsection] = useState([]);
     const [detailRule, setDetailRule] = useState(null);
-    
     const [isOpen, setIsOpen] = useState(false);
-    
-
 
     useEffect(()=> {
         fetch(`https://www.dnd5eapi.co${url}`)
@@ -24,41 +20,22 @@ function RulesType({ name, url, id }) {
         .then(data => {setDetailRule(data)} )
         .catch(error => console.error(error))
     }
-
-    
-
-        return (
+    return (
         <>         
-        <h2 className="title-adventuring-h2" key={name} id={id}>{name}</h2> 
-    
-            
-
-       
+            <h2 className="title-adventuring-h2" key={name} id={id}>{name}</h2> 
             {subsection.map((element) =>
-             <ul key={element.url}>
-
-                
-                <button className="rule-button"type="button" onClick={()=> (handleClick(element.url))}  >
-                 
-                <section><h3>{element.name}</h3>
-                <p className="plus">+</p>
-                </section>
-                {isOpen && detailRule && detailRule.name === element.name && <div><p className="detailRule">{detailRule.desc}</p></div>}  
-                
-                </button>
-
-
-                </ul>
-        
-             
-            )
-                  
-        
-        }
-           
- 
-    </>
-);
+                <ul key={element.url}>
+                    <button className="rule-button"type="button" onClick={()=> (handleClick(element.url))}  > 
+                        <section>
+                            <h3>{element.name}</h3>
+                            <p className="plus">+</p>
+                        </section>
+                        {isOpen && detailRule && detailRule.name === element.name && <div><p className="detailRule">{detailRule.desc}</p></div>}  
+                    </button>
+                </ul>          
+            )}            
+        </>
+    );
 }
     
 RulesType.propTypes = 
